8f6d1275a73cbd7d0fbb30530aced67d355f30a0
OmarTALBI <omarettaleby@gmail.com>
2023-07-17 15:39:14 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main  - A programm that use puts function
+*Return: 0 (empty)
+*/
+int main (void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return(0);
+}
