6d65fc34c5ed8b43228a0c9c5a39d6281ddc20b2
OmarTALBI <omarettaleby@gmail.com>
2023-07-19 16:30:51 -0700

    none

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,22 @@
 #include "main.h"
 /**
-* main function + print via putchar
-*return 0
+*main just for entry
+*main return none
+*for return is void empty
 */
+
 int main(void)
+
 {
-_putchar('_');
-_putchar('p');
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
-return (0);
+
+	_putchar("_");
+	_putchar("p");
+	_putchar("u");
+	_putchar("t");
+	_putchar("c");
+	_putchar("h");	
+	_putchar("a");
+	_putchar("r");
+	return (0);
+
 }
