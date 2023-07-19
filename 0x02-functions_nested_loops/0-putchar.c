5a5f2ca39c76b220f7eea4aae016937a16401f88
OmarTALBI <omarettaleby@gmail.com>
2023-07-18 17:30:52 -0700

    _ptchar function in C

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,18 @@
 #include "main.h"
-#include <unistd.h>
-**
-*a lot of documentation about putchar
-*writes a single character to the standard output stream, stdout. Its prototype is as follows: int putchar (int character) The character to be printed is fed into the function as an argument, and if the writing is successful, the argument character is returned.
-*return no empty
+/**
+* main function + print via putchar
+*return 0
 */
-int _putchar(char c)
+int main(void)
 {
-	return (write(1, &c, 1));
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
