404b08a73eb905921cffea1e21c07c7d28f3d0af
OmarTALBI <omarettaleby@gmail.com>
2023-07-24 16:49:16 -0700

    pointers in c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* takes a pointer to an int as parameter and updates the value it points to to 98
+* points to 98
+* @ n: integer input pointer
+* 
+* return void empty
+*/
+void reset_to_98(int *n)
+{
+	*n=98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+Pointers sur C
