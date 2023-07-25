98c527ef9248ad271b9b2661291d695ad01e869d
OmarTALBI <omarettaleby@gmail.com>
2023-07-24 17:06:42 -0700

    pointers in c

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -10,3 +15,5 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
+
+#endif /* MAIN_H */
