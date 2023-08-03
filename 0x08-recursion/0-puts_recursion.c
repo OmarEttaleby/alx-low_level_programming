0584046695aade3b1832328c95a4f498497f39cd
OmarTALBI <omarettaleby@gmail.com>
2023-08-03 15:57:48 -0700

    la recursion in C

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+*_puts_recursion* il écrit un char suivi new line
+*@s: points sur le string
+*return empty or void
+*/
+
+void _puts_recursion(char *s)
+{
+	if (*s== '\0')
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar(*s);
+		_puts_recursion(s+1);
+	}
+}
+
+
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+la récursion dans C
