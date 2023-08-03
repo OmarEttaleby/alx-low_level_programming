cdf5e9f9530017dbf5b9158002a4e67b60dc82a6
OmarTALBI <omarettaleby@gmail.com>
2023-08-03 15:59:41 -0700

    la recursion in C

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,14 +8,14 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s== '\0')
+	if (*s == '\0')
 	{
 		_putchar('\n');
 	}
 	else
 	{
 		_putchar(*s);
-		_puts_recursion(s+1);
+		_puts_recursion(s + 1);
 	}
 }
 
