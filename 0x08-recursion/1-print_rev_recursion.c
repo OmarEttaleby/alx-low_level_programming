1e1ca6855d9e1c86e40c4ba31f6b7b45d758ee57
OmarTALBI <omarettaleby@gmail.com>
2023-08-03 16:09:35 -0700

    recursion in C

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_print_rev_recursion - affiche  string dans sens différent 
+ *@s: string that we need to print in reverse
+ *Return: no return
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
