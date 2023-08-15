c6d304145f9f65082cefb1c7171f681c5104b9b9
OmarTALBI <omarettaleby@gmail.com>
2023-08-15 14:52:01 -0700

    struc dog in C

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - Structure represent chien
+ * @name: prenom
+ * @age: âge chien
+ * @owner: propritaire
+ *
+ * Description: represent chier proprietatire.
+ */
+
+struct dog {
+    char *name;
+    float age;
+    char *owner;
+};
