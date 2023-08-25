019a022d7c1769df8fcdde240af14f7c23c31239
OmarTALBI <omarettaleby@gmail.com>
2023-08-25 03:13:01 -0700

    print lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,23 +8,47 @@
  */
 size_t print_list(const list_t *h)
 {
-    size_t node_count = 0;
+	size_t node_count = 0;
 
-    while (h)
-    {
-        if (!h->str)
-        {
-            printf("[0] (nil)\n");
-        }
-        else
-        {
-            printf("[%d] %s\n", h->len, h->str);
-        }
+	while (h)
+	{
+		if (!h->str)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", h->len, h->str);
+		h = h->next;
+		node_count++;
+	}
 
-        h = h->next;
-        node_count++;
-    }
-
-    return node_count;
+	return node_count;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
