5daaadb925ceea978c375da5fb9cdc2294d55d2c
OmarTALBI <omarettaleby@gmail.com>
2023-08-29 16:57:09 -0700

    liks

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,14 +8,15 @@
  */
 size_t print_listint(const listint_t *h)
 {
-const listint_t *cursor = h;
-size_t count = 0;
+	const listint_t *cursor = h;
+	size_t count = 0;
 
-while (cursor != NULL)
-{
-printf("%d\n", cursor->n);
-count += 1;
-cursor = cursor->next;
+	while (cursor != NULL)
+	{
+		printf("%d\n", cursor->n);
+		count++;
+		cursor = cursor->next;
+	}
+	return (count);
 }
-return (count);
-}
+
