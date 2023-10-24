b52390dac4307bb5f3dd47ee28db26d3ca01ea7c
OmarTALBI <omarettaleby@gmail.com>
2023-10-24 15:47:36 -0700

    test

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,22 +1,21 @@
 #include "lists.h"
 
 /**
- * print_listint - imprime tous les éléments d'une liste chaînée
- * @h: liste chaînée de type listint_t à imprimer
+ * print_listint - affiche tous les éléments d'une liste.
+ * @h: tête de la liste.
  *
- * Return: nombre de nœuds
+ * Retour: nombre de nœuds.
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t num = 0;  // initialiser le compteur de nœuds
+	size_t nnodes = 0;
 
-    while (h)  // tant que le nœud courant n'est pas NULL
-    {
-        printf("%d\n", h->n);  // imprimer la valeur du nœud courant
-        num++;  // augmenter le compteur de nœuds
-        h = h->next;  // passer au nœud suivant
-    }
-
-    return (num);  // retourner le nombre total de nœuds
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		nnodes++;
+	}
+	return (nnodes);
 }
 
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,39 +1,50 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_
+#define _LISTS_
 
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * struct listint_s - liste chaînée simple
- * @n: entier
- * @next: pointe vers le nœud suivant
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
  *
- * Description: structure de nœud de liste chaînée simple
- * pour le projet Holberton
+ * Description: singly linked list node structure
+ * for ALX project
  */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
-// Prototypes des fonctions
-size_t print_listint(const listint_t *h); // Imprimer la liste
-size_t listint_len(const listint_t *h); // Obtenir la longueur de la liste
-listint_t *add_nodeint(listint_t **head, const int n); // Ajouter un nœud au début
-listint_t *add_nodeint_end(listint_t **head, const int n); // Ajouter un nœud à la fin
-void free_listint(listint_t *head); // Libérer la liste
-void free_listint2(listint_t **head); // Libérer la liste en utilisant un double pointeur
-int pop_listint(listint_t **head); // Supprimer le premier nœud
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index); // Obtenir le nœud à un index spécifique
-int sum_listint(listint_t *head); // Somme des données de tous les nœuds
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n); // Insérer un nœud à un index spécifique
-int delete_nodeint_at_index(listint_t **head, unsigned int index); // Supprimer un nœud à un index spécifique
-listint_t *reverse_listint(listint_t **head); // Inverser la liste
-size_t print_listint_safe(const listint_t *head); // Imprimer la liste de manière sécurisée (pour les listes contenant des boucles)
-size_t free_listint_safe(listint_t **h); // Libérer la liste de manière sécurisée (pour les listes contenant des boucles)
-listint_t *find_listint_loop(listint_t *head); // Trouver une boucle dans la liste
+/**
+ * struct listp_s - singly linked list
+ * @p: pointers of nodes
+ * @next: points to the next node
+ *
+ * Description: singly linked list of pointers
+ */
+typedef struct listp_s
+{
+	void *p;
+	struct listp_s *next;
+} listp_t;
+
+size_t print_listint(const listint_t *h);
+size_t listint_len(const listint_t *h);
+listint_t *add_nodeint(listint_t **head, const int n);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+void free_listint2(listint_t **head);
+int pop_listint(listint_t **head);
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index);
+int sum_listint(listint_t *head);
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+listint_t *reverse_listint(listint_t **head);
+size_t print_listint_safe(const listint_t *head);
+size_t free_listint_safe(listint_t **h);
+listint_t *find_listint_loop(listint_t *head);
 
 #endif
-
