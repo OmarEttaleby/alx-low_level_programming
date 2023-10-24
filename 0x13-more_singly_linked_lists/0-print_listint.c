ea6eec79005250338568d92e2e79e89c7cea45ac
OmarTALBI <omarettaleby@gmail.com>
2023-10-24 04:39:06 -0700

    test

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,21 +2,21 @@
 
 /**
  * print_listint - imprime tous les éléments d'une liste chaînée
- * @h: liste chaînée à imprimer
+ * @h: liste chaînée de type listint_t à imprimer
  *
  * Return: nombre de nœuds
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t num = 0;
+    size_t num = 0;  // initialiser le compteur de nœuds
 
-    while (h)
+    while (h)  // tant que le nœud courant n'est pas NULL
     {
-        printf("%d\n", h->n);
-        num++;
-        h = h->next;
+        printf("%d\n", h->n);  // imprimer la valeur du nœud courant
+        num++;  // augmenter le compteur de nœuds
+        h = h->next;  // passer au nœud suivant
     }
 
-    return num;
+    return (num);  // retourner le nombre total de nœuds
 }
 
